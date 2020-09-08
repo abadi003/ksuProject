82b2574a45798493dd0a0f69aaa6479644f9e1a5
abadi PC <alaneef.kiils@gmail.com>
2020-09-08 13:41:06 +0300

    .

--- models/index.js
+++ models/index.js
@@ -7,15 +7,15 @@ var env = process.env.NODE_ENV || "development";
 var config = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
 console .log(process.env.DB_HOST)
 const dbSocketAddr = "172.17.0.1:3306".split(":")
-var sequelize = new Sequelize(mysql.createPool({
+var sequelize = new Sequelize({
     user: "root", // e.g. 'my-db-user'
     password: "12a n f012345", // e.g. 'my-db-password'
     database: "book", // e.g. 'my-database'
     host: dbSocketAddr[0], // e.g. '127.0.0.1'
     port: dbSocketAddr[1], // e.g. '3306'
     // ... Specify additional properties here.
-    ...config
-  }));
+    config
+  });
 var db = {};
 
 
