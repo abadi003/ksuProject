26c9232e87c2cc71247de9bbc08a71e79c0ac1da
abadi PC <alaneef.kiils@gmail.com>
2020-09-11 10:12:31 +0300

    .

--- dataBase.js
+++ dataBase.js
@@ -7,12 +7,6 @@ var con = mysql.createConnection({
 });
 con.connect()
 exports.sql = function sql(msg , callback) {
-    var con = mysql.createConnection({
-        host: "127.0.0.1",
-        user: "root",
-        password: "12a n f012345"
-    });
-    con.connect()
         con.query("SELECT sha1(\"" + msg + "\")", function (err, result, fields) {
             if (err) throw err;
                 callback(result[0]["sha1(\"" + msg + "\")"])
